dbe68bd97d18ac6bd4a006c39b4d343b197cac28
Vitamin1899 <vitalimarinich@gmail.com>
2015-12-07 14:37:43 +0300

    created uploader; modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Testfile::Application.routes.draw do
-  get "resumes/index"
-  get "resumes/new"
-  get "resumes/create"
-  get "resumes/destroy"
+  resources :resumes, only: [:index, :new, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'resumes#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
